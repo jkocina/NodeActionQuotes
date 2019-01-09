d48548a66be9820c51744bedbf08e3aa0b6b6767
Jacob K <jkocina.jr@gmail.com>
2019-01-09 00:45:14 +0000

    Adding routing and wgetting a template

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -3,6 +3,14 @@ var app = express();
 
 var port = process.env.PORT; //this is a cloud 9 environment variable to find the port hosted on
 
+app.get('/', function(req, res){
+    res.send('Bench Press');
+});
+
+app.get('/routing', function(req, res){
+    res.send('a route for routing');
+});
+
 app.listen(port, function(err){
     console.log("Server is running on port: " + port);
 });
