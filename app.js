56f390601c443158cfaecdcc7042c1d572b3f0fe
Jacob K <jkocina.jr@gmail.com>
2019-02-04 02:13:58 +0000

    commenting

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express() //referencing the express framework
 var eventRouter = express.Router() //Instantiating the event router
 var fs = require('fs') //for parsing json files
 var port = process.env.PORT //this is a cloud 9 environment variable to find the port hosted on
+var qouteJson = require("./public/data/quotes.json") //Quote data file
 
 // adding static resource directories to the scope of app.js 
 app.use(express.static('public'))
@@ -10,6 +11,11 @@ app.use(express.static('public'))
 //Templating set-up
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
+
+//Telling the server which port to listen on
+app.listen(port, function(err){
+    console.log("Server is running on port: " + port)
+})
 
 //Setting up routing 
 app.use('/quotes', eventRouter)
@@ -24,12 +30,6 @@ eventRouter.route('/add') //Routing to the add a quote page
     .get(function(req, res){
         res.send('Hello add a quote')
     })
-
-
-//json file for parsing
-//var contents = fs.readFileSync("./public/data/quotes.txt");
-var qouteJson = require("./public/data/quotes.json")
-
 
 //Resonding to root request using EJS templating
 app.get('/', function(req, res){
@@ -51,7 +51,7 @@ app.get('/quotes', function(req, res){
     
     res.render('quotes', {
         list: ['first val','second val','third val'],
-        nav: [{ link : "http://systemslife.net/" , text : "Systems Life"}],
+        nav: [{ link : "http://systemslife.net/" , text : "Systems Life"}]
               /* Future features
               { link : "home", text : "Home" },
               { link : "sign-Up", text : "Sign-Up"},
@@ -60,22 +60,15 @@ app.get('/quotes', function(req, res){
     })
 })
 
-app.listen(port, function(err){
-    console.log("Server is running on port: " + port)
-})
-
-
-//This function is passed to the index page through the templating engine
+//This helper function will test whether show or movie is populated and will pass to index.ejs
 function movieOrShow(movie, show)  {
 
     if (show != "" )  {
         
         return show
-        
     } else if (movie != "" ) {
         
         return movie
-        
     } else {
         
         return "No movie listed"
