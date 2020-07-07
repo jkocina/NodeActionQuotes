ff0058c05d1fdc474d0979920f46f7ab0e8ea322
Jacob Kocina <9373074+jkocina@users.noreply.github.com>
2020-07-06 20:49:36 -0500

    updating the navbar to the newer site redirect

--- src/routes/quoteRouter.js
+++ src/routes/quoteRouter.js
@@ -6,7 +6,7 @@ quoteRouter.route('/') //Routing to the root of the quote page
     .get(function(req, res){
       res.render('quotes', {
         list: ['first val','second val','third val'],
-        nav: [{ link : "http://systemslife.net/" , text : "Systems Life"}]
+        nav: [{ link : "http://jacobkocina.com/" , text : "Systems Life"}]
               /* Future features
               { link : "home", text : "Home" },
               { link : "sign-Up", text : "Sign-Up"},
